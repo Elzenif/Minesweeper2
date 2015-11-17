31ffc7714434052bfee5bc4ffd38f43329250f8d
Germain Shigo <germain.shigo@ensimag.grenoble-inp.fr>
2015-11-17 15:10:13 +0100

    New folders

---
+++ src/gameGrid/AbstractGrid.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   AbstractGrid.cpp
+ * Author: shigog
+ * 
+ * Created on 17 novembre 2015, 15:09
+ */
+
+#include "AbstractGrid.h"
+
+AbstractGrid::AbstractGrid() {
+}
+
+AbstractGrid::AbstractGrid(const AbstractGrid& orig) {
+}
+
+AbstractGrid::~AbstractGrid() {
+}
+
---
+++ src/gameGrid/AbstractGrid.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   AbstractGrid.h
+ * Author: shigog
+ *
+ * Created on 17 novembre 2015, 15:09
+ */
+
+#ifndef ABSTRACTGRID_H
+#define	ABSTRACTGRID_H
+
+class AbstractGrid {
+public:
+    AbstractGrid();
+    AbstractGrid(const AbstractGrid& orig);
+    virtual ~AbstractGrid();
+private:
+
+};
+
+#endif	/* ABSTRACTGRID_H */
+
