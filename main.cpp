81553e96c8ef8c856143f8a29072393221988ec4
Germain Shigo <germain.shigo@ensimag.grenoble-inp.fr>
2015-11-17 15:00:35 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: shigog
+ *
+ * Created on 17 novembre 2015, 14:52
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello World";
+    int i;
+    cin >> i;
+    
+    return 0;
+}
+
